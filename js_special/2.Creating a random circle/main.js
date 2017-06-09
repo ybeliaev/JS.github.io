63dad149947d631e0a0f8c5ee80c95dc4ce146c2
Yura Beliaev <ybeliaev@gmail.com>
2017-06-09 19:27:05 +0300

    add 3 folder

--- js_special/2.Creating a random circle/main.js
+++ js_special/2.Creating a random circle/main.js
@@ -1,37 +1,51 @@
-window.onload = function(){
-	//создание окружности диаметром от 50 до 150px;
-
-	
-
-	//создадим рандомный цвет
-	function generateColor() {
-		return '#' + Math.floor( Math.random() * 16777215 ).toString(16);
-	}
-
-	
-	
-	function generateCircle(){
-		//создание самого эллемента и добавление к нему класса
-		var div = document.createElement('div');
-		div.className = 'circle';
-		document.body.appendChild(div);
-
-		//генерация случайного цвета и диаметра
-		var width = Math.floor(Math.random() * 100 + 50);
-		
-		//присвоение свойств
-		div.style.backgroundColor = generateColor();
-		div.style.width = width + 'px';
-		div.style.height = width + 'px';
-
-	}
-
-	generateCircle();
+window.onload = function() {
+        //создание окружности диаметром от 50 до 150px;
 
 
 
+        //создадим рандомный цвет
+        function generateColor() {
+            return '#' + Math.floor(Math.random() * 16777215).toString(16);
+        }
+
+
+
+        function generateCircle() {
+            //создание самого эллемента и добавление к нему класса
+            var div = document.createElement('div');
+            div.className = 'circle';
+            document.body.appendChild(div);
+
+            //генерация случайного цвета и диаметра
+            var width = Math.floor(Math.random() * 100 + 50);
+
+            //присвоение свойств
+            div.style.backgroundColor = generateColor();
+            div.style.width = width + 'px';
+            div.style.height = width + 'px';
+
+        }
+
+        //generateCircle();
+
+        //функция цикличного вызова. Почему колличество окружностей зависит от i и цикл не останавливается???
+        var i = 0;
+
+        function showCircle() {
+            generateCircle();
+
+            if (i < 1) {
+                i++;
+                setInterval(showCircle, 1000);
+
+
+            }
+        }
+
+        showCircle();
 
 
 
 
-}//end 
+
+    } //end
---
+++ js_special/3.Palindrom/main.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+        //создать массив чисел палиндронов от 0 до 1000
+        var palindArr = [];
+        for (var i = 0; i <= 1000; i++) {
+            if (isPalindrom(i)) {
+                palindArr.push(i);
+            }
+        }
+
+        //функция, определяющая число палиндром
+
+        function isPalindrom(num) {
+
+            var oldNum = num;
+            var newNum = 0;
+            while (num > 0) {
+                newNum = newNum * 10 + num % 10;
+                num = parseInt(num / 10);
+            }
+            if (oldNum == newNum) {
+                return true;
+            } else return false;
+        }
+
+
+
+    } //end
