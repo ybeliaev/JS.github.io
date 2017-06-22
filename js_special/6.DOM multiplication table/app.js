816458b59101096bf5b7b548b1a30a6f4531bf86
Yura Beliaev <ybeliaev@gmail.com>
2017-06-22 22:24:07 +0300

    Add end multitable

--- js_special/6.DOM multiplication table/app.js
+++ js_special/6.DOM multiplication table/app.js
@@ -14,7 +14,7 @@
 
 // Table
 //**************************************************************************** */
-
+/*
 var table = document.createElement("table");
 for (var i = 1; i <= 10; i++) {
     var tr = document.createElement("tr"); // create table row
@@ -26,3 +26,34 @@ for (var i = 1; i <= 10; i++) {
     table.appendChild(tr);
 }
 document.body.appendChild(table);
+*/
+// Функция конструктор. Показать таблицу, вызвав функцию show() из функции-конструктора
+//*********************************************************************************** */
+
+function multiTable(x, y) {
+    this.init_ = function() {
+        this.table = document.createElement("table");
+        for (var i = 1; i <= x; i++) {
+            var tr = document.createElement("tr"); // create table row
+            for (var k = 1; k <= y; k++) {
+                var td = document.createElement("td");
+                td.appendChild(document.createTextNode(i * k));
+                tr.appendChild(td);
+            }
+            this.table.appendChild(tr);
+        }
+    }
+    this.show = function() {
+        document.body.appendChild(this.table);
+    }
+    this.init_();
+}
+var newTable = new multiTable(5, 5);
+newTable.show();
+
+(new multiTable(15, 15)).show(); // как вариант без переменной
+
+// можно скрипт выше вписать в анонимную функцию - тогда, все объекты внутри будут недоступны извне
+(function() {
+    //код
+})();
