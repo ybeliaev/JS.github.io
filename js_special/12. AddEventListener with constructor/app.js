2fe08913f0babccbe667d11d5b889faf376e3f99
Yura Beliaev <ybeliaev@gmail.com>
2017-06-24 20:38:46 +0300

    12 three

--- js_special/12. AddEventListener with constructor/app.js
+++ js_special/12. AddEventListener with constructor/app.js
@@ -1,14 +1,33 @@
 window.addEventListener("DOMContentLoaded", init);
 
+function Button(name) {
+    this.name = name;
+    this.show = function() {
+        document.body.appendChild(this.btnElem);
+    }
+}
+
+this.attachEvent_ = function() {
+    this.btnElem.addEventListener("click", this.btnClick_);
+}
+this.btnClick_ = function() {
+    console.log(this.name);
+}
+
 function BtnCreate() {
     this.show = function() {
         document.body.appendChild(this.btnElem);
     }
+
+
     this.init_ = function() {
         this.btnElem = document.createElement("div");
         this.btnElem.className = "demo";
+
+        var textNode = document.createTextNode(this.name);
+        this.btnElem.appendChild(textNode);
     }
-    this.init_(); // нижнее подчёркивание говорит, что функция исспользуется только внутри конструктора
+    this.init_(); // нижнее подчёркивание говорит, что функция используется только внутри конструктора
 }
 
 function init() {
