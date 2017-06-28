328b351c8110d59d628f47c1afd7c05ba734d4d3
Yura Beliaev <ybeliaev@gmail.com>
2017-06-28 13:34:51 +0300

    14 forms first

--- js_special/14. Forms/app.js
+++ js_special/14. Forms/app.js
@@ -1,2 +1,22 @@
+window.addEventListener("DOMContentLoaded", init);
 
+function init() {
+    var elem = document.getElementById("demo");
+    var massagesBox = document.getElementById("massegesbox");
+    // keyup - отпустили клавишу
+    //keydown - нажали
+    //keypress - нажали и держим
 
+    // ev - информация о случившимся событиии
+    elem.addEventListener("keyup", function(ev) {
+        console.log(this.value);
+        console.log(ev.keyCode); // вывод номера нажатой клавиши
+
+        if (ev.keyCode == 13) {
+            var newTextLine = document.createElement("p");
+            newTextLine.appendChild(document.createTextNode(this.value));
+            massagesBox.appendChild(newTextLine);
+            this.value = "";
+        }
+    })
+} // end init
