f00dc94784c7914d65b5867e29e1551da00dda56
Yura Beliaev <ybeliaev@gmail.com>
2017-06-28 11:41:52 +0300

    13_1

--- js_special/13_1.Lightbox_modern/app.js
+++ js_special/13_1.Lightbox_modern/app.js
@@ -1,6 +1,15 @@
 window.addEventListener("DOMContentLoaded", init);
 
-var blackpanel, imges;
+var blackpanel,
+    imges,
+    container;
+
+function closeLightbox() {
+    blackpanel.innerHTML = "";
+    blackpanel.style.display = "none";
+    //Отписать у объекта blackpanel от события "click" функцию closeLightbox
+    blackpanel.removeEventListener("click", closeLightbox);
+}
 
 
 function init() {
@@ -8,13 +17,14 @@ function init() {
     blackpanel = document.getElementById('blackpanel');
     // HTMLCollection
     imges = document.getElementsByClassName('myImg');
-    console.log(imges);
+    container = document.getElementsByClassName("container")[0];
+
     for (var i = 0; i < imges.length; i++) {
         imges[i].addEventListener('click', function() {
             blackpanel.style.display = "flex";
             console.log(this); // указывает на картинку
             console.log(this.src); // указывает на путь к картинке
-
+            //формируем тег
             var bigImg = document.createElement("img");
             bigImg.src = this.src;
             bigImg.style.margin = "auto";
@@ -23,16 +33,18 @@ function init() {
 
             blackpanel.appendChild(bigImg);
 
-            blackpanel.addEventListener("click", function() {
-                this.removeChild(bigImg);
-                this.style.display = "none";
-
-            })
+            // навешиваем на панель событие
+            blackpanel.addEventListener("click", closeLightbox);
         })
     }
-    // можно вынести за цикл
-    // blackpanel.addEventListener("click", function() {
-    //             this.innerHTML = "";
-    //             this.style.display = "none";
-    //})
+
+    // тройной клик на элементе меняет фон на жёлтый
+    var count = 0;
+    container.addEventListener("click", function() {
+        count++;
+        if (count == 3) {
+            this.style.background = "yellow";
+        }
+
+    })
 }
